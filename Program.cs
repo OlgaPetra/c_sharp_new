612b56cd8f72308302dd49647627b320b6913ed3
Olya <pradicha@mail.ru>
2022-08-10 22:12:12 +0300

    задача 1 _число в квадрат

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая на вход принимает число,на выходе выдает его квадрат
+
+
+Console.Clear();
+
+Console.Write("Введите число :");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number*number;
+//number *= number; 
+//int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+
+
+
